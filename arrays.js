cbe3145698e79abff9d7fbc24fb88cb965437de2
Benz <btagle@abenson.com>
2025-07-09 09:12:29 +0800

    Updated and added ver2

---
+++ arrays.js
@@ -0,0 +1,232 @@
+const vouchers = [
+  //
+  // 1st voucher
+  {
+    id: "A",
+    logoSrc: "img/kolin_logo.png",
+    value: "P300",
+    dateRange: "Oct 1 - Oct 10, 2025",
+    code: "BENZPOGI",
+    terms1: "See applicable items",
+    terms2: "min. spend of P50,000",
+    backgroundColor: "blue",
+    // Modals:
+    modalTitle: "Voucher Test",
+    modalDate: "June 27 - 30, 2025",
+    modalSections: [
+      {
+        contentType: "list",
+        title:
+          "Get up to P300 OFF with a minimum purchase of P50,000 on all applicable brands",
+        content: [
+          "Midea Small Appliance",
+          "Dowell Small Appliance",
+          "Samsung Small Appliance",
+          "Panasonic Small Appliance",
+        ],
+      },
+      {
+        contentType: "links",
+        title: "Where to avail",
+        content: [
+          { text: "abenson.com", href: "https://www.abenson.com" },
+          { text: "Abenson Viber Community", href: "#" },
+          { text: "Abenson Hotline: (02)8811-8811", href: "tel:0288118811" },
+        ],
+      },
+      {
+        contentType: "list",
+        title: "Terms & Conditions",
+        content: [
+          "Promo code is valid for one time use only.",
+          "Promo terms and conditions are subject to change without prior notice.",
+          "Applicable for cash/straight online transactions only.",
+        ],
+      },
+    ],
+  },
+  //
+  // 2nd voucher
+  {
+    id: "B",
+    logoSrc: "img/kolin_logo.png",
+    value: "P500",
+    dateRange: "Oct 1 - Oct 10, 2025",
+    code: "BENZPOGI",
+    terms1: "See applicable items",
+    terms2: "min. spend of P50,000",
+    backgroundColor: "orange",
+    // Modals:
+    modalTitle: "Voucher Test",
+    modalDate: "June 27 - 30, 2025",
+    modalSections: [
+      {
+        contentType: "list",
+        title:
+          "Get up to P300 OFF with a minimum purchase of P50,000 on all applicable brands",
+        content: [
+          "Midea Small Appliance",
+          "Dowell Small Appliance",
+          "Samsung Small Appliance",
+          "Panasonic Small Appliance",
+        ],
+      },
+      {
+        contentType: "links",
+        title: "Where to avail",
+        content: [
+          { text: "abenson.com", href: "https://www.abenson.com" },
+          { text: "Abenson Viber Community", href: "#" },
+          { text: "Abenson Hotline: (02)8811-8811", href: "tel:0288118811" },
+        ],
+      },
+      {
+        contentType: "list",
+        title: "Terms & Conditions",
+        content: [
+          "Promo code is valid for one time use only.",
+          "Promo terms and conditions are subject to change without prior notice.",
+          "Applicable for cash/straight online transactions only.",
+        ],
+      },
+    ],
+  },
+  //
+  // 3rd voucher
+  {
+    id: "C",
+    logoSrc: "img/kolin_logo.png",
+    value: "P300",
+    dateRange: "Oct 1 - Oct 10, 2025",
+    code: "BENZPOGI",
+    terms1: "See applicable items",
+    terms2: "min. spend of P50,000",
+    // Modals:
+    modalTitle: "Voucher Test",
+    modalDate: "June 27 - 30, 2025",
+    backgroundColor: "red",
+    modalSections: [
+      {
+        contentType: "list",
+        title:
+          "Get up to P300 OFF with a minimum purchase of P50,000 on all applicable brands",
+        content: [
+          "Midea Small Appliance",
+          "Dowell Small Appliance",
+          "Samsung Small Appliance",
+          "Panasonic Small Appliance",
+        ],
+      },
+      {
+        contentType: "links",
+        title: "Where to avail",
+        content: [
+          { text: "abenson.com", href: "https://www.abenson.com" },
+          { text: "Abenson Viber Community", href: "#" },
+          { text: "Abenson Hotline: (02)8811-8811", href: "tel:0288118811" },
+        ],
+      },
+      {
+        contentType: "list",
+        title: "Terms & Conditions",
+        content: [
+          "Promo code is valid for one time use only.",
+          "Promo terms and conditions are subject to change without prior notice.",
+          "Applicable for cash/straight online transactions only.",
+        ],
+      },
+    ],
+  },
+  //
+  // 4th voucher
+  {
+    id: "D",
+    logoSrc: "img/kolin_logo.png",
+    value: "P300",
+    dateRange: "Oct 1 - Oct 10, 2025",
+    code: "BENZPOGI",
+    terms1: "See applicable items",
+    terms2: "min. spend of P50,000",
+    backgroundColor: "red",
+    // Modals:
+    modalTitle: "Voucher Test",
+    modalDate: "June 27 - 30, 2025",
+    modalSections: [
+      {
+        contentType: "list",
+        title:
+          "Get up to P300 OFF with a minimum purchase of P50,000 on all applicable brands",
+        content: [
+          "Midea Small Appliance",
+          "Dowell Small Appliance",
+          "Samsung Small Appliance",
+          "Panasonic Small Appliance",
+        ],
+      },
+      {
+        contentType: "links",
+        title: "Where to avail",
+        content: [
+          { text: "abenson.com", href: "https://www.abenson.com" },
+          { text: "Abenson Viber Community", href: "#" },
+          { text: "Abenson Hotline: (02)8811-8811", href: "tel:0288118811" },
+        ],
+      },
+      {
+        contentType: "list",
+        title: "Terms & Conditions",
+        content: [
+          "Promo code is valid for one time use only.",
+          "Promo terms and conditions are subject to change without prior notice.",
+          "Applicable for cash/straight online transactions only.",
+        ],
+      },
+    ],
+  },
+  //
+  // 5th voucher
+  {
+    id: "test",
+    logoSrc: "img/kolin_logo.png",
+    value: "P300",
+    dateRange: "Oct 1 - Oct 10, 2025",
+    code: "BENZPOGI",
+    terms1: "See applicable items",
+    terms2: "min. spend of P50,000",
+    backgroundColor: "red",
+    // Modals:
+    modalTitle: "Voucher Test",
+    modalDate: "June 27 - 30, 2025",
+    modalSections: [
+      {
+        contentType: "list",
+        title:
+          "Get up to P300 OFF with a minimum purchase of P50,000 on all applicable brands",
+        content: [
+          "Midea Small Appliance",
+          "Dowell Small Appliance",
+          "Samsung Small Appliance",
+          "Panasonic Small Appliance",
+        ],
+      },
+      {
+        contentType: "links",
+        title: "Where to avail",
+        content: [
+          { text: "abenson.com", href: "https://www.abenson.com" },
+          { text: "Abenson Viber Community", href: "#" },
+          { text: "Abenson Hotline: (02)8811-8811", href: "tel:0288118811" },
+        ],
+      },
+      {
+        contentType: "list",
+        title: "Terms & Conditions",
+        content: [
+          "Promo code is valid for one time use only.",
+          "Promo terms and conditions are subject to change without prior notice.",
+          "Applicable for cash/straight online transactions only.",
+        ],
+      },
+    ],
+  },
+];
